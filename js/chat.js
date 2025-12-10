2d67b9bfd738e0af25c329c92144928ff8999cea
2yassin <yalbkwr50@gmail.com>
2025-12-10 19:48:09 +0300

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,1 @@
+window.$crisp=[];window.CRISP_WEBSITE_ID="bbe61027-00be-4ddf-a008-a81e37943d59";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
